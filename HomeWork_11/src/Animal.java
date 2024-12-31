c611a634876fd8135c7af3d87021304e75fd575e
Garrnur <artsiombarysik@gmail.com>
2024-12-31 10:58:32 +0300

    Задание 1, юез расширения задачи

--- HomeWork_11/src/Animal.java
+++ HomeWork_11/src/Animal.java
@@ -1,4 +1,43 @@
+/** Создать классы Собака и Кот с наследованием от класса Животное.
+Все животные могут бежать и плыть. В качестве параметра каждому методу передается длина препятствия.
+Результатом действия будет печать в консоль.
+У каждого животного есть ограничение на действия (бег: кот 200 метров, собака 500 метров;
+плавание: кот не умеет плавать, собака 10 метров)
+Добавить подсчет созданных котов, собак и животных.
+ */
 public class Animal {
     private String name;
+    private static int animalClassCount = 0;
+    private static int totalCount = 0;
+    // Конструктор класса Animal
+    public Animal(String name) {
+        this.name = name;
+        if (this.getClass() == Animal.class) {
+            animalClassCount++;
+        }
+        totalCount++;
 
-}
+    }
+    // Геттеры класса Animal
+    public String getName() {
+        return name;
+    }
+    public static int getAnimalClassCount() {
+        return animalClassCount;
+    }
+    public static int getTotalCount() {
+        return totalCount;
+    }
+    // Сеттер класса Animal
+    public void setName(String name) {
+        this.name = name;
+    }
+    // Базовый метод бега
+    public void run (int distance) {
+            System.out.println(this.name + " пробежал " + distance + " метров.");
+        }
+    // Базовый метод плавания
+    public void swim (int distance) {
+            System.out.println(this.name + " проплыл " + distance + " метров.");
+        }
+    }
--- HomeWork_11/src/Main.java
+++ HomeWork_11/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Animal test = new Animal("Йося");
+        test.run(10);
+        test.swim(0);
+        Cat josep = new Cat("Йося","Хаки");
+        josep.run(201);
+        josep.swim(10);
+        Dog dog1 = new Dog("Шарик","синий");
+        dog1.run(-10);
+        dog1.run(499);
+        dog1.run(501);
+        dog1.swim(-99);
+        dog1.swim(9);
+        dog1.swim(99);
+        System.out.println("животные " + Animal.getAnimalClassCount());
+        System.out.println("общее " + Animal.getTotalCount());
+        System.out.println("коты " + Cat.getCatClassCount());
+        System.out.println("собаки " + Dog.getDogClassCount());
+        josep.checkSatiety();
     }
 }
